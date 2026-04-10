2f4a4a98f7bd1ddf89e9b3d6cead329f1d4a29c7
Andrew McLinden <amclinden@mtech.edu>
2026-04-10 09:44:17 -0600

    completed assignment

--- src/main.asm
+++ src/main.asm
@@ -2,6 +2,8 @@
 %define int_21h call _interupt_21h ; Alias to simulate DOS interrupt behavior
 global  _main
 extern  _interupt_21h
+extern _scanf
+extern _printf
 
 
 section .data
@@ -14,9 +16,56 @@ section .data
 section .text
 _main:
 
-    
+    call _input
+
+    call heron
+
+    call _output
 
     ; Exit program with return code 0 (AH=00h)
     mov ah, 0
     mov al, 0 ; Set return code to 0
     int_21h
+
+    _input:
+        mov edx, S
+        push edx
+        mov edx, fmt
+        push edx
+
+        call _scanf
+        add esp, 8
+    ret
+
+    _output:
+        mov edx, [x + 4]
+        push edx
+
+        mov edx, [x]
+        push edx
+
+        mov edx, fmt
+        push edx
+
+        call _printf
+        add esp, 12
+    ret
+
+    heron:
+        mov ecx, 10
+
+        .loop1:
+            fninit
+            fld qword [half]
+            fld qword [S]
+            fld qword [x]
+            
+            fdiv
+            fld qword [x]
+
+            fadd
+            fmul
+            fst qword [x]
+
+            loop .loop1
+    ret
