f93743f0d9a4691f7f10f415f6c51ab74908a40c
ARMclinden <amclinden@mtech.edu>
2026-04-18 14:59:37 -0600

    completed assignment except for wrap around (ask)

--- src/main.asm
+++ src/main.asm
@@ -4,31 +4,46 @@ global  _main
 extern  _interupt_21h
 extern _scanf
 extern _printf
+extern _exit
 
 
 section .data
     input_text db 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 0
     padding db 0, 0, 0, 0, 0, 0, 0, 0
     
-    shift db 2 
-    fmt db "%s"
+    shift db 2
+    
+    spaces db 32, 32, 32, 32, 32, 32, 32, 32
+    _low db 96, 96, 96, 96, 96, 96, 96, 96
+    _up db 122, 122, 122, 122, 122, 122, 122, 122
+    _lowc db 64, 64, 64, 64, 64, 64, 64, 64
+    _upc db 90, 90, 90, 90, 90, 90, 90, 90
 
 section .text
 _main:
-
-    sub esp, 8
-    add esp, 8
     
+    push input_text
+
     call _encode
 
-    call _print
+    push input_text
 
-    ; Exit program with return code 0 (AH=00h)
-    mov ah, 0
-    mov al, 0 ; Set return code to 0
-    int_21h
+    call _printf
+
+    push 0
+    call _exit
 
 _encode:
+
+    pop eax
+    push ebx
+    push ebp
+
+    mov ebp, esp
+    mov ebx, [ebp+8]
+
+    pop ebp
+
     ; ceasar cipher with shift of 2 using mmx registers
     ; create a full mmx register with the shift value (2) repeated in each byte
     movq mm1, [shift]
@@ -37,43 +52,56 @@ _encode:
     punpcklwd mm1, mm1
     punpcklwd mm1, mm1
 
-    ; load input text into mmx register 8 bytes at a time
-    ; add 2 to each byte, wrapping to the beginning of the alphabet if necessary
-    ; store the result back to memory
+    ; mask of spaces to prevent them from being changed
 
+    push esi
     xor esi, esi
-    movq mm2, [padding] ; Load padding into mm2 to handle the last few bytes of input_text
     .loop1:
-        movq mm0, [input_text + esi]
+        ; declaration
+        movq mm0, [ebx + esi]
+        pcmpeqb mm4, mm4
 
-        
+        ; check lower bound for uppercase
+        movq mm2, [_lowc]
+        movq mm3, mm0
+        pcmpgtb mm3, mm2
+        movq mm5, mm1
+        pand mm5, mm3
 
-        paddb mm0, mm7
+        ;check upper bound for uppercase
+        movq mm2, [_upc]
+        movq mm3, mm0
+        pcmpgtb mm3, mm2
+        pxor mm3, mm4
+        pand mm5, mm3
 
-        movq [input_text + esi], mm0
+        ; check lower bound for lowercase
+        movq mm2, [_low]
+        movq mm3, mm0
+        pcmpgtb mm3, mm2
+        movq mm6, mm1
+        pand mm6, mm3
+
+        ;check upper bound for lowercase
+        movq mm2, [_up]
+        movq mm3, mm0
+        pcmpgtb mm3, mm2
+        pxor mm3, mm4
+        pand mm6, mm3
+
+        por mm5, mm6
+
+        paddb mm0, mm5
+         
+        movq [ebx + esi], mm0
 
         add esi, 8
         cmp esi, 445
         jb .loop1
-ret
-_print:
-    pop eax
-    ; push encoded text onto the stack 4 bytes at at time
-    xor esi, esi
-    mov esi, 444
-    xor edx, edx
-    push edx
-
-    .loop2:
-        push [input_text + esi]
-
-        sub esi, 4
-        cmp esi, 0
-        ja .loop2
     
-    push fmt
-    
-    call _printf
+    emms
 
+    pop esi
+    pop ebx
     push eax
 ret
