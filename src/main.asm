191b767cd478fb9dd55d8aa60f7ede19d9d66c5a
Andrew McLinden <amclinden@mtech.edu>
2026-04-20 10:07:22 -0600

    Updated template

--- src/main.asm
+++ src/main.asm
@@ -4,15 +4,23 @@ global  _main
 extern  _interupt_21h
 extern _scanf
 extern _printf
+extern _exit
 
+; Andrew Ryan McLinden
+; CSCI 361 Spring 2026
+; Assignment class 31
+; I acknowledge that I have worked on this assignment independently,
+; except where explicitly noted and referenced. Any collaboration or
+; use of external resources has been properly cited.
+; I am fully aware of the consequences of academic dishonesty and
+; agree to abide by the university's academic integrity policy.
 
 section .data
 
 
 section .text
 _main:
-
+    
     ; Exit program with return code 0 (AH=00h)
-    mov ah, 0
-    mov al, 0 ; Set return code to 0
-    int_21h
+    push 0
+    call _exit
